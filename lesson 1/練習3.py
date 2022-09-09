a43d09cf15d063491f15a579c3eed9c2ba63602f
Kingsley1116 <93391504+Kingsley1116@users.noreply.github.com>
2022-09-09 17:30:27 +0800

    Delete lesson 1 directory

--- lesson 1/練習3.py
+++
@@ -1,8 +0,0 @@
-# 取得變數的資料類型
-name = "我的姓名是xxx"
-height = 1.60
-weight = 44
-print("name類型 = " + str(type(name)))
-print("height類型 = " + str(type(height)))
-print("weight類型 = " + str(type(weight)))
-print(f"{name}, 高度是{height}米, 重量是{weight}公斤")
